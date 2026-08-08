a516f92babaf8faf30624dc72d59a82111cc728d
tajurh8-cmd <tajurh8@gmail.com>
2026-08-08 18:14:54 +0700

    Update push-config.js

--- push-config.js
+++ push-config.js
@@ -1,4 +1,4 @@
 // Firebase Web Push public VAPID key.
 // Aman disimpan di frontend karena ini PUBLIC key, bukan secret.
 // Firebase Console > Project Settings > Cloud Messaging > Web Push certificates.
-export const FCM_VAPID_PUBLIC_KEY = "PASTE_VAPID_PUBLIC_KEY_HERE";
+export const FCM_VAPID_PUBLIC_KEY = "BNHNMAphICy5ycw8WJcXJeofZl9c4P-SHn8OEeh0tbCOYjDjAPusZf_dy-0K7U3V79SXWrJEZccb7Maw0RR2Myg";
